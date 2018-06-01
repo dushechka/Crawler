2c10560c229d470a829d0b301d577f2c8ca52420
dushechka <aaafdv@gmail.com>
2018-06-01 16:19:11 +1100

    Add ugly nonworking db interaction code

--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -1,5 +1,32 @@
+import java.sql.*;
+
 public class WebCrawler {
-    public static void main(String[] args) {
-        
+
+    private static final String DB_ADRESS = "jdbc:mysql://localhost:3306/searchandratewords";
+    private static final String USER_NAME = "crawler";
+    private static final String PASSWORD = "123";
+
+    public static void main(String[] args) throws Exception {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(DB_ADRESS, USER_NAME, PASSWORD);
+            if (conn == null) {
+                System.out.println("No database connection!");
+                System.exit(0);
+            }
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM pages WHERE siteID=1");
+            while (rs.next()) {
+                System.out.println(rs.getRow() + ". " + rs.getString("URL")
+                        + "\t" + rs.getString("foundDateTime"));
+            }
+            stmt.close();
+        } catch (SQLException exc) {
+            exc.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+        }
     }
 }
