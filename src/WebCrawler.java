9e0a29e1282d38e765ae88d8b22c3f99281683b3
dushechka <aaafdv@gmail.com>
2018-06-02 18:45:36 +1100

    Refactor WebCrawler and RatesDatabase classes

--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -1,27 +1,47 @@
 import dbs.DBFactory;
 import dbs.sql.RatesDatabase;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.*;
 import java.util.Set;
 
 public class WebCrawler {
     private static final String ROBOTS_TXT_APPENDIX = "/robots.txt";
 
-    public static void main(String[] args) throws Exception {
-        try {
-            RatesDatabase ratesDb = DBFactory.getRatesDb();
-            Set<Integer> sites = ratesDb.getSitesWithSinglePages();
-            for (Integer siteId : sites) {
-                String address = ratesDb.getSiteAddress(siteId);
-                if (address != null) {
-                    System.out.println(address);
-                    String robotsAddress = address + ROBOTS_TXT_APPENDIX;
-                    ratesDb.insertRowInPages(robotsAddress, siteId,
-                                                new Timestamp(System.currentTimeMillis()));
+    /**
+     * Inserts links to the robots.txt file
+     * for sites, that have only one link in DB.
+     *
+     * @param ratesDb   Database to work with
+     * @throws SQLException if database can't
+     *                      execute some of the queries.
+     */
+    private static void insertLinksToRobotsPages(RatesDatabase ratesDb) throws SQLException {
+        Set<Integer> sites = ratesDb.getSitesWithSinglePages();
+        for (Integer siteId : sites) {
+            String address = ratesDb.getSiteAddress(siteId);
+            if (address != null) {
+                System.out.println(address);
+                String robotsAddress = address + ROBOTS_TXT_APPENDIX;
+                try {
+                    ratesDb.insertRowInPages(new URL(robotsAddress), siteId,
+                            new Timestamp(System.currentTimeMillis()));
+                } catch (MalformedURLException exc) {
+                    System.out.println("Found malformed URL for a site! SiteID =" + siteId);
+                    System.out.println("Can't insert robots.txt link.");
                 }
             }
-        } catch (SQLException exc) {
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            RatesDatabase ratesDb = DBFactory.getRatesDb();
+            insertLinksToRobotsPages(ratesDb);
+        } catch (Exception exc) {
             exc.printStackTrace();
         }
     }
+
 }
--- src/dbs/sql/RatesDatabase.java
+++ src/dbs/sql/RatesDatabase.java
@@ -16,12 +16,20 @@ public class RatesDatabase {
         this.conn = conn;
     }
 
+    /**
+     * Get rows from "Pages" table with counts of pages per site.
+     */
     private ResultSet getPageCounts() throws SQLException {
         Statement stmt = conn.createStatement();
         return stmt.executeQuery(
                 "SELECT siteID, COUNT(*) FROM pages GROUP BY siteID");
     }
 
+    /**
+     * Get collection of sites with only one page from "Pages" table.
+     * @return  Site ID's
+     * @throws SQLException
+     */
     public Set<Integer> getSitesWithSinglePages() throws SQLException {
         Set<Integer> sites = new HashSet<>();
         ResultSet rs = getPageCounts();
@@ -34,6 +42,11 @@ public class RatesDatabase {
         return sites;
     }
 
+    /**
+     * Get collection of sites with more than one page in "Pages" table.
+     * @return  Site ID's
+     * @throws SQLException
+     */
     public Set<Integer> getSitesWithMultiplePages() throws SQLException {
         Set<Integer> sites = new HashSet<>();
         ResultSet rs = getPageCounts();
@@ -46,6 +59,16 @@ public class RatesDatabase {
         return sites;
     }
 
+    /**
+     * Searches for a link to the given <code>siteId</code>
+     * in database and returns address to this site in form:
+     * "protocol://host.domain". For example: "https://example.com".
+     * @param siteId
+     * @return Site address or null, if there's no such
+     *         <code>siteId</code> or link for this site
+     *         in database is malformed.
+     * @throws SQLException
+     */
     public @Nullable String getSiteAddress(int siteId) throws SQLException {
         String result = null;
         PreparedStatement stmt = conn.prepareStatement(
@@ -58,29 +81,31 @@ public class RatesDatabase {
                 result = url.getProtocol() + PROTOCOL_DELIMITER + url.getHost();
             }
         } catch (MalformedURLException exc) {
-            System.out.println("Malformed URL. SiteID = " + siteId);
+            System.out.println("Malformed URL: siteID = " + siteId);
         }
         stmt.close();
         return result;
     }
 
-    public boolean insertRowInPages(String url, int siteId,
+    /**
+     * Inserts new row in "Pages" table.
+     *
+     * @param url           URL of the page being inseted
+     * @param siteId        ID of the page's site in DB
+     * @param lastScanDate  Timestamp when page was scanned
+     *                      last time or null, if never was
+     * @throws SQLException
+     */
+    public void insertRowInPages(URL url, int siteId,
                                     @Nullable Timestamp lastScanDate) throws SQLException {
-        try {
             // checking, if url is malformed
             System.out.println("Creating URL from: " + url);
-            URL pageUrl = new URL(url);
             PreparedStatement stmt = conn.prepareStatement(
                                         "INSERT INTO pages (URL, siteID, lastScanDate) VALUES (?, ?, ?)" );
-            stmt.setString(1, url);
+            stmt.setString(1, url.toString());
             stmt.setInt(2, siteId);
             stmt.setTimestamp(3, lastScanDate);
             stmt.execute();
             stmt.close();
-            return true;
-        } catch (MalformedURLException exc) {
-            exc.printStackTrace();
-        }
-        return false;
     }
 }
