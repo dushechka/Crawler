5096ba68d40c70c6ea76e66c9bf1a73dd8cebeae
dushechka <aaafdv@gmail.com>
2018-06-05 18:07:55 +1100

    Add SitemapNodeIterable class

---
+++ src/main/java/com/allendowney/thinkdast/SitemapNodeIterable.java
@@ -0,0 +1,13 @@
+package com.allendowney.thinkdast;
+
+import org.jsoup.nodes.Node;
+
+import java.util.Iterator;
+
+public class SitemapNodeIterable implements Iterable<Node> {
+
+    @Override
+    public Iterator<Node> iterator() {
+        return null;
+    }
+}
